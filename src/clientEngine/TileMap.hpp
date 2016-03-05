66faaade0138062154eb6cd00240bc110cd9d1ab
Jose C.Rubio <jose.rubio@mindmaze.ch>
2016-03-05 19:56:00 +0100

    Adds player position in tilemap

--- src/clientEngine/TileMap.hpp
+++ src/clientEngine/TileMap.hpp
@@ -1,11 +1,20 @@
 #pragma once
 #include <memory>
 
+struct TilePosition {
+	int tilex;
+	int tiley;
+};	
+
 class TileMap {
 public:
 
-	typedef std::shared_ptr<TileMap> Ptr;
 	
+	typedef std::shared_ptr<TileMap> Ptr;
+
+private:
+
+	TilePosition _playerPosition;
 	
 	
 };
