86bb5855d004ed5691f62e2acc837d0f873f2d5f
byfron <josecrubio82@gmail.com>
2017-01-26 18:09:22 +0100

    Adds two new basic components

---
+++ src/components/AnimationComponent.hpp
@@ -0,0 +1,46 @@
+#pragma once
+
+#include <graphics/Animation.hpp>
+#include <graphics/AnimationFactory.hpp>
+
+// shall the graphic elements be organized per texture?
+class AnimationComponent {
+public:
+	AnimationComponent(const std::string & config_file) : 
+		m_animation(config_file) {
+		m_animation.init();
+	}
+
+	void update(float delta ) {
+		m_animation.update(delta);
+	}
+
+	pumpkin::Animation & getAnimation2D() {
+		return m_animation;
+	}
+
+private:
+
+	uint32_t m_resource_id;
+	pumpkin::Animation m_animation;
+};
+
+
+class AnimationSystem : public System<AnimationSystem> {
+public:
+	void update(EntityManager & em, /*EventManager &evm*/ float delta ) {
+		em.each<AnimationComponent, PlayerState>([delta](Entity entity,
+								 AnimationComponent &anim,
+								 PlayerState & state) {
+//			 if (state.isDirty()) {
+				 anim.getAnimation2D().setPosition(state.getPosition());
+				 anim.getAnimation2D().switchToAnim(state.getAnimationId());
+//			 }
+
+			 anim.update(delta);
+		 });
+	}
+
+private:
+
+};
---
+++ src/components/PlayerComponent.hpp
@@ -0,0 +1,135 @@
+#pragma once
+
+#include <entities/System.hpp>
+#include <utils/Interval.hpp>
+#include <pumpkin.hpp>
+
+// TODO:
+/*
+  * Fix installation of shaders/textures
+  * Clean up a bit/ write diagrams
+  * Implement simple map+two entities and move them with two clients
+*/
+enum class PlayerAction {
+	Idle,
+	Walking,
+	Rolling,
+	Shooting
+};
+
+enum class CardinalDirection {
+	East,
+	North,
+	West,
+	South,
+	// NorthEast,
+	// NorthWest,
+	// SouthWest,
+	// SouthEast,
+};
+
+class PlayerState {
+public:
+	PlayerState(float moveSpeed) : m_moveSpeed(moveSpeed) {
+		m_position = Eigen::Vector3f(0.0f,1.5f,0.0f);//initial position (cfg!)
+		m_direction = CardinalDirection::South;
+		m_action = PlayerAction::Walking;
+	}
+
+	void computeOrientation() {
+
+		//if (USER_PRESSING_FIRE)
+		if (false) {
+			MultiInterval<float> mint(Interval<float>(-M_PI, M_PI), 4);
+			int idx = mint.indexContains(pumpkin::InputManager::m_mouse_angle);
+			m_direction = CardinalDirection(idx);
+		}
+		else {
+
+		}
+	}
+
+	void computeAction() {
+		if (pumpkin::InputManager::m_keys != 0) {
+			m_action = PlayerAction::Walking;
+		}
+	}
+
+	uint32_t getAnimationId() {
+
+		if (m_action == PlayerAction::Walking) {
+			//TODO: read from a table
+			if (m_direction == CardinalDirection::East) {
+return 0;//AnimationId::RIGHT;
+			}
+
+if (m_direction == CardinalDirection::West) {
+return 1;//AnimationId::LEFT;
+			}
+
+			if (m_direction == CardinalDirection::North) {
+				return 2;//AnimationId::TOP;
+			}
+			if (m_direction == CardinalDirection::South) {
+				return 3;//AnimationId::DOWN;
+			}
+		}
+	}
+
+	Eigen::Vector3f getMoveDirection() {
+
+		Eigen::Vector3f direction = Eigen::Vector3f(0.0f, 0.0f, 0.0f);
+
+		if (pumpkin::InputManager::m_keys & KEY_MOVE_UP) {
+			direction += pumpkin::GraphicsEngine::camera().getForwardDir();
+			m_direction = CardinalDirection::North;
+		}
+		if (pumpkin::InputManager::m_keys & KEY_MOVE_DOWN) {
+			direction += -pumpkin::GraphicsEngine::camera().getForwardDir();
+			m_direction = CardinalDirection::South;
+		}
+		if (pumpkin::InputManager::m_keys & KEY_MOVE_RIGHT) {
+			direction += -pumpkin::GraphicsEngine::camera().getRightDir();
+			m_direction = CardinalDirection::East;
+		}
+		if (pumpkin::InputManager::m_keys & KEY_MOVE_LEFT) {
+			direction += pumpkin::GraphicsEngine::camera().getRightDir();
+			m_direction = CardinalDirection::West;
+		}
+
+		return direction;
+
+	}
+
+	Eigen::Vector3f getPosition() const {
+		return m_position;
+	}
+
+	void update(float dt) {
+
+//		m_action = PlayerAction::Idle;
+		m_position += getMoveDirection()* m_moveSpeed * dt;
+		computeAction();
+		computeOrientation();
+
+	}
+
+private:
+
+//	TileIndex m_tile;
+
+	float m_moveSpeed;
+	Eigen::Vector3f m_position;
+	CardinalDirection m_direction;
+	PlayerAction m_action;
+};
+
+class StateSystem : public System<StateSystem> {
+
+	void update(EntityManager & em, /*EventManager &evm*/ float delta ) {
+		em.each<PlayerState>([delta](Entity entity, PlayerState &state) {
+				state.update(delta);
+ 	        });
+	}
+
+};
