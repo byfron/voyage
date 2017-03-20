1322d5d733821cfc25685978eddfb503b9f2577b
Jose C. Rubio <jose.rubio@mindmaze.ch>
2017-03-20 21:22:47 +0100

    Adds settings file in python script

---
+++ python-utils/world_generation/settings.py
@@ -0,0 +1,8 @@
+
+class Settings:
+
+    def __init__(self):
+        self.root_project = '/home/rubio/Projects/voyage'
+        self.path_textures = self.root_project + '/pumpkin/data/textures'
+        self.path_shaders = self.root_project + '/pumpkin/data/shaders'
+        self.texture_compiler = self.root_project + '/pumpkin/bgfx/.build/linux64_gcc/bin/texturecRelease'
