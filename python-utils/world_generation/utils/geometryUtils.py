1e7758fc8ac9f319e98d04ad34e22cf4e3921e00
Jose C. Rubio <jose.rubio@mindmaze.ch>
2017-03-23 22:56:01 +0100

    Adds python geometry utils

---
+++ python-utils/world_generation/utils/geometryUtils.py
@@ -0,0 +1,40 @@
+#
+# line segment intersection using vectors
+# see Computer Graphics by F.S. Hill
+#
+from numpy import *
+def perp( a ) :
+    b = empty_like(a)
+    b[0] = -a[1]
+    b[1] = a[0]
+    return b
+
+# line segment a given by endpoints a1, a2
+# line segment b given by endpoints b1, b2
+# return
+def find_intersection(a1,a2, b1,b2) :
+    da = a2-a1
+    db = b2-b1
+    dp = a1-b1
+    dap = perp(da)
+    denom = dot( dap, db)
+    num = dot( dap, dp )
+    return (num / denom.astype(float))*db + b1
+
+
+def ccw(A,B,C):
+    return (C[1]-A[1]) * (B[0]-A[0]) > (B[1]-A[1]) * (C[0]-A[0])
+
+# Return true if line segments AB and CD intersect
+def intersect(A,B,C,D):
+    return ccw(A,C,D) != ccw(B,C,D) and ccw(A,B,C) != ccw(A,B,D)
+
+def is_intersecting_segment_polygon(segment, polygon):
+
+    for v_idx in range(len(polygon)-1):
+        v1 = polygon[v_idx]
+        v2 = polygon[v_idx+1]
+        if intersect(segment[0], segment[1], v1, v2):
+            return True
+
+    return False
